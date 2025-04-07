19c8293889dd12c69e8f69f32b0dfb0bcfb3146e
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-04-08 00:11:04 +0600

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,13 @@
-import { Button, Stack } from "@mui/material";
+import Navbar from "@/components/Navbar/Navbar";
+
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl mt-36 text-center font-lora font-bold">Pixora</h1>
       <div>
-        <Stack spacing={2} direction="row">
-          <Button
-            sx={{
-              backgroundColor: "#FF5722",
-              color: "white",
-              "&:hover": {
-                backgroundColor: "#E64A19",
-              },
-            }}
-          >
-            Click Me
-          </Button>
-        </Stack>
+        <Navbar />
       </div>
+      <h1 className="text-5xl mt-36 text-center font-lora font-bold">Pixora</h1>
     </div>
   );
 }
