7c719b4882b6199c7645a96a7c9fd78299381de1
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-04-07 22:09:24 +0600

    install material UI library for customize this website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import { Button, Stack } from "@mui/material";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-5xl mt-36 text-center font-lora font-bold">Pixora</h1>
+      <div>
+        <Stack spacing={2} direction="row">
+          <Button
+            sx={{
+              backgroundColor: "#FF5722",
+              color: "white",
+              "&:hover": {
+                backgroundColor: "#E64A19",
+              },
+            }}
+          >
+            Click Me
+          </Button>
+        </Stack>
+      </div>
     </div>
   );
 }
