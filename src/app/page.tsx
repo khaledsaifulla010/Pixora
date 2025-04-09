3ca90d0030a1a430d0718c58c63fb4cd878bf569
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-04-09 11:15:23 +0600

    show upload form in UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Navbar from "@/components/Navbar/Navbar";
+import UploadImage from "@/components/UploadImage/UploadImage";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-5xl mt-36 text-center font-lora font-bold">Pixora</h1>
+
+      <div className="mt-24 mb-36 max-w-[500px] mx-auto">
+        <UploadImage />
+      </div>
     </div>
   );
 }
